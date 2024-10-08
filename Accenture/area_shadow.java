fe56e834111fb8b9447b011287d1b28a03324762
Arun-1102 <arunganesan877@gmail.com>
2024-10-08 14:27:47 +0530

    new one

--- Accenture/area_shadow.java
+++ Accenture/area_shadow.java
@@ -5,7 +5,7 @@ public class area_shadow {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int d=scan.nextInt();
-        int a=(int) ((Math.pow(d,2))*3.14);
+        int a=(int)(Math.pow(d,2)*3.14);
         System.out.print(a);
     }
 }
